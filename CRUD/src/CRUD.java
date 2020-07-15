8ba7e67c9454bfda0ba59666a176f85601b453d2
Wasek Rahman <31708476+WasekRahman@users.noreply.github.com>
2020-07-15 15:37:50 -0400

    Enhancement: Java Code
    Added

---
+++ CRUD/src/CRUD.java
@@ -0,0 +1,51 @@
+import java.io.*; 
+import java.util.*; 
+import com.opencsv.CSVWriter; 
+
+public class CRUD {
+	static int option;
+	
+	public static void menu()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("What do you want to do?\n"
+				+"1. Create a new data file\n"
+				+ "2. Read an existing data file\n"
+				+"3. Update an existing data file\n"
+				+"4. Delete an existing data file\n"
+				+"5. Exit");
+		
+		option = sc.nextInt();
+		
+	}
+	public static void exit()
+	{
+		System.out.println("Exiting program..");
+	}
+	
+	public static void main(String[] args) throws IOException 
+	{ 
+		Scanner sc = new Scanner(System.in);
+		while(true){
+			menu();
+			if(option == 1)
+			{
+				System.out.println("Enter filename to create: ");
+				String filename = sc.nextLine();
+				Create.createFile(filename);
+			}
+			if(option == 2)
+			{
+				System.out.println("Enter filename to read: ");
+				String filename = sc.nextLine();
+				Read.readFile(filename);
+			}
+			if(option == 5)
+			{
+				exit();
+				break;
+			}
+		}
+	} 
+
+} 
---
+++ CRUD/src/Create.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+import com.opencsv.CSVWriter;
+
+public class Create {
+
+	public static void createFile(String filePath) 
+	{ 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Creating " + filePath +"...");
+		filePath = "./" + filePath +".csv";
+		File tmpDir = new File(filePath);
+		boolean exists = tmpDir.exists();
+		if(exists == true)
+		{
+			System.out.println("File exists already. Do you want to overwrite it?\n1. Yes\n2. No");
+			int option = sc.nextInt();
+			if(option==1)
+			{
+				System.out.println("Overwriting..");
+			}
+			if(option==2)
+			{
+				return;
+			}
+			
+			
+		}
+		
+		// first create file object for file placed at location 
+		// specified by filepath 
+		File file = new File(filePath); 
+		try { 
+			// create FileWriter object with file as parameter 
+			FileWriter outputfile = new FileWriter(file); 
+
+			// create CSVWriter object filewriter object as parameter 
+			CSVWriter writer = new CSVWriter(outputfile); 
+
+			// adding header to csv 
+			System.out.println("How many headers do you want to create?");
+			int num = sc.nextInt();
+			String[] header = new String[num];
+			header[0] = sc.nextLine();
+			for(int i=0;i<num;i++)
+			{
+				String data = sc.nextLine();
+				header[i] = data;
+			}
+			
+			writer.writeNext(header);
+			System.out.println("SUccessfully written!");
+			// closing writer connection 
+			writer.close(); 
+		} 
+		catch (IOException e) { 
+			// TODO Auto-generated catch block 
+			e.printStackTrace(); 
+		} 
+	} 
+}
